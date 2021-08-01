87002ca37476ffe598120c4efeb097d618c3b387
rpgarde <rpgarde@gmail.com>
2021-08-01 18:47:38 +1000

    MVP working

--- index.js
+++ index.js
@@ -2,18 +2,104 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require(('./utils/generateMarkdown.js'));
 
-
-// TODO: Include packages needed for this application
+// validator to ensure that all fields have inputs
+const validator = (input) => {
+    if(input.length===0){  
+        return 'Please provide an input';
+    }
+    else{
+    return true;
+    }
+}
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+        type: "input",
+        message: "What is your project's title?",
+        name: "project",
+        validate: validator
+    },
+    {
+        type: "input",
+        message: "What is the repo name of this project?",
+        name: "repo",
+        validate: validator
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username",
+        validate: validator
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+        validate: validator
+    },
+    {
+        type: "input",
+        message: "Please write a description about the project",
+        name: "description",
+        validate: validator
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: [
+            "MIT",
+            "GPLv3",
+            "Apache"
+        ],
+        validate: validator
+    },
+    {
+        type: "input",
+        message: "What command should be used to install dependencies?",
+        name: "dependencies",
+        validate: validator
+    },
+    {
+        type: "input",
+        message: "What command should the user use to run tests?",
+        name: "test",
+        validate: validator
+    }, 
+    {
+        type: "input",
+        message: "How should the user use the repo?",
+        name: "usage",
+        validate: validator
+    },
+    {
+        type: "input",
+        message: "How can users contribute to the repo?",
+        name: "contributions",
+        validate: validator
+    }
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          throw err;
+        }
+        console.log("File created!");
+      });
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    generateMarkdown()
+    inquirer
+    .prompt(questions)
+    .then(
+        (data) => {
+            writeToFile("README.MD",generateMarkdown(data))
+        }
+    )
+
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,11 +14,43 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-console.log('test');
-renderLicenseBadge();
-  //   return `# ${data.title}
+return `# ${data.project}
+https://img.shields.io/github/license/${data.username}/${data.repo}
 
-// `;
+## Description
+${data.description}
+
+## Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+Run the following code to install dependencies:\n
+        ${data.dependencies}
+
+## Usage
+${data.usage}
+
+## License
+This project uses ${data.license} license.
+
+## Contributing
+${data.contributions}
+
+## Tests
+Run the following code in your console to run tests\n
+        ${data.test}
+
+## Questions
+For any further questions, reach out to ${data.email}
+
+`
 }
 
 module.exports = generateMarkdown;
+
+//{project,repo,username,email,description,license,dependencies,test,usage,contributions}
