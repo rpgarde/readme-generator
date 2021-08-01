687657bd75e4057f161effb67f7c4d81a737529e
rpgarde <rpgarde@gmail.com>
2021-08-01 17:34:14 +1000

    initial commit, filed linked

---
+++ index.js
@@ -0,0 +1,20 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require(('./utils/generateMarkdown.js'));
+
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    generateMarkdown()
+}
+
+// Function call to initialize app
+init();
